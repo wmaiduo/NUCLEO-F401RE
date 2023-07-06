4428097f1ee39fc92aa7d64c31ada4e39eec3869
Michael Wu <mwu@armstrongfluidtechnology.com>
2023-07-06 14:24:44 -0400

    1_GPIOInputOutput Attempt

--- 1_GPIOInputOutput/Debug/Src/subdir.mk
+++ 1_GPIOInputOutput/Debug/Src/subdir.mk
@@ -7,27 +7,30 @@
 C_SRCS += \
 ../Src/main.c \
 ../Src/syscalls.c \
-../Src/sysmem.c 
+../Src/sysmem.c \
+../Src/system_stm32f4xx.c 
 
 OBJS += \
 ./Src/main.o \
 ./Src/syscalls.o \
-./Src/sysmem.o 
+./Src/sysmem.o \
+./Src/system_stm32f4xx.o 
 
 C_DEPS += \
 ./Src/main.d \
 ./Src/syscalls.d \
-./Src/sysmem.d 
+./Src/sysmem.d \
+./Src/system_stm32f4xx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F401RETx -DSTM32F4 -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Inc -I"C:/Users/mwu/Projects/NUCLEO-F401RE/1_GPIOInputOutput/Drivers/CMSIS/Include" -I"C:/Users/mwu/Projects/NUCLEO-F401RE/1_GPIOInputOutput/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/mwu/Projects/NUCLEO-F401RE/1_GPIOInputOutput/Drivers/STM32F4xx_HAL_Driver/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32f4xx.cyclo ./Src/system_stm32f4xx.d ./Src/system_stm32f4xx.o ./Src/system_stm32f4xx.su
 
 .PHONY: clean-Src
 
--- 1_GPIOInputOutput/Src/main.c
+++ 1_GPIOInputOutput/Src/main.c
@@ -1,9 +1,59 @@
+#include "stm32f4xx_hal.h"
 
-int counter;
+#define BTN_PORT	GPIOC
+#define BTN_PIN		GPIO_PIN_13
+#define LED_PORT	GPIOA
+#define LED_PIN		GPIO_PIN_5
+
+//BTN = PC13	,BUS = AHB1EN bit0
+//LED = PA5		,BUS = AHB1EN bit2
+
+void pc13_btn_init(void);
+void pa5_led_init(void);
+
+uint8_t buttonStatus;
 
 int main() {
+	HAL_Init();
+	pc13_btn_init();
+	pa5_led_init();
+
 	while (1) {
-		counter++;
+		buttonStatus = HAL_GPIO_ReadPin (BTN_PORT, BTN_PIN);
+		HAL_GPIO_WritePin (LED_PORT, LED_PIN, buttonStatus);
 	}
 }
 
+void pc13_btn_init(void) {
+	//1. Configure PA5 as output
+
+	//enable clock
+	GPIO_InitTypeDef 	GPIO_InitStruct = {0};
+
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	GPIO_InitStruct.Pin = GPIO_PIN_13;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+	HAL_GPIO_Init (GPIOA, &GPIO_InitStruct);
+}
+
+void pa5_led_init(void) {
+	//2. Configure PC13 AS input pin
+
+	//enable clock for PC13
+	GPIO_InitTypeDef 	GPIO_InitStruct = {0};
+
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	GPIO_InitStruct.Pin = BTN_PIN;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void SysTick_Handler(void) {
+	HAL_IncTick();
+}
